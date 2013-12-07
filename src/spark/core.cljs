e69fd36b2c61ee4724be014f43ac7694c874fd17
Gert Goet <gert@thinkcreate.nl>
2013-12-07 18:53:02 +0100

    Show usage

--- src/spark/core.cljs
+++ src/spark/core.cljs
@@ -1,6 +1,16 @@
 (ns spark.core)
 
 (defn start [& _]
-  (println "YOLO"))
+  (println "
+      USAGE:
+        spark [-h] VALUE,...
+
+      EXAMPLES:
+        spark 1 5 22 13 53
+        ▁▁▃▂█
+        spark 0,30,55,80,33,150
+        ▁▂▃▄▂█
+        echo 9 13 5 17 1 | spark
+        ▄▆▂█▁"))
 
 (set! *main-cli-fn* start)
