d3be46a5ffffef188aa755a70843dc6a9f3a8441
Gert Goet <gert@thinkcreate.nl>
2013-12-07 23:41:05 +0100

    Calculate graph

--- src/spark/core.cljs
+++ src/spark/core.cljs
@@ -17,7 +17,15 @@
         ▄▆▂█▁"))
 
 (defn print-graph [args]
-  (println "▁▂█▅▂"))
+  (let [ticks '(\▁  \▂  \▃  \▄  \▅  \▆  \▇  \█)
+        numbers (map int args)
+        lower (apply min numbers)
+        upper (apply max numbers)
+        tick-diff (int (/ (* (- upper lower) 256) (dec (count ticks))))
+        tick-pos (fn [x] (int (/ (* (- x lower) 256) tick-diff)))]
+    ;(println "min: " lower ", max: " upper ", diff: " tick-diff)
+    ;(println (map #(tick-pos %) numbers))
+    (println (apply str (map #(nth ticks (tick-pos %)) numbers)))))
 
 (defn start [& args]
   (if (print-usage? args)
